e7205cbd6bc5eebd49f6a8490f7ee2941578d605
Angadpreet Nagpal <angadpreet.nagpal@gmail.com>
2019-04-09 18:06:59 -0700

    Delete input.R

--- input.R
+++
@@ -1,7 +0,0 @@
-train <- read.csv('./train.csv', stringsAsFactors = F)
-test  <- read.csv('./test.csv', stringsAsFactors = F)
-
-full  <- bind_rows(train, test) # bind training & test data
-
-# check data
-str(full)
