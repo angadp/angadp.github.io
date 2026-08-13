98eb85a0d44b8395aaba47a4df3c17205bd2ae89
Angadpreet Nagpal <angad@Mac-86.lan>
2026-08-13 16:57:28 +0530

    Update portfolio: Flipkart SDE 3, Amazon end date, IIM MBA, SEO title

---
+++ precache-manifest.223eb3d0cc561f910c99d8ca8753d60b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ec4109c7048896a177952ff51cfd2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c636871e60e66a204980",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "d64c4344da49ad206b83",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "fe1bb98337e401c4c0c9",
+    "url": "/static/css/main.ea1d9968.chunk.css"
+  },
+  {
+    "revision": "17b75516797f96ab07ed",
+    "url": "/static/js/2.6ab4433a.chunk.js"
+  },
+  {
+    "revision": "1f879fb0070366d9cb43b95b93ae861f",
+    "url": "/static/js/2.6ab4433a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c636871e60e66a204980",
+    "url": "/static/js/3.49aff7a4.chunk.js"
+  },
+  {
+    "revision": "d64c4344da49ad206b83",
+    "url": "/static/js/4.253cebb1.chunk.js"
+  },
+  {
+    "revision": "fe1bb98337e401c4c0c9",
+    "url": "/static/js/main.64431f92.chunk.js"
+  },
+  {
+    "revision": "d31565cf293a896eca05",
+    "url": "/static/js/runtime-main.288fbe8b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "3d5f270dc17065c37c42ebe90e373a87",
+    "url": "/static/media/CCNA.3d5f270d.png"
+  },
+  {
+    "revision": "fd629e4e2afaacaca4e8415507df8d04",
+    "url": "/static/media/Flipkart.fd629e4e.png"
+  },
+  {
+    "revision": "be979f1d1c5548272a5cd2e61e572bd7",
+    "url": "/static/media/IIM.be979f1d.png"
+  },
+  {
+    "revision": "b30fa773027ab018a5ce6f90b76cda70",
+    "url": "/static/media/MNCP.b30fa773.jpeg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8f1b5d3605048c34b3bf482ac03807fc",
+    "url": "/static/media/Thapar.8f1b5d36.jpg"
+  },
+  {
+    "revision": "a6fe9fe04f25d42bff326ced38856813",
+    "url": "/static/media/USC.a6fe9fe0.jpg"
+  },
+  {
+    "revision": "2d248e3be0f04d6266ce9a20cd35dfb7",
+    "url": "/static/media/Zynga.2d248e3b.png"
+  },
+  {
+    "revision": "e63635b3b6ca01de375b078a81318b08",
+    "url": "/static/media/amazon-logo.e63635b3.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
